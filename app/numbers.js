5bcf729f2951af6e47839aaf135350603c01368e
reedm26 <reedm26@yahoo.com>
2019-11-12 15:32:28 -0700

    finished / but want to clarify

--- app/numbers.js
+++ app/numbers.js
@@ -5,7 +5,7 @@ function add(x, y) {
 
 // Write a function that returns the Cubed value of x
 function cubed(x) {
-  return Math.pow(x, 3);
+  return x ** 3;
 }
 
 // Write a function that will divide two numbers, and will format the returned value
@@ -17,10 +17,16 @@ function divideWholey(x, y) {
 // Write a function that takes in a number and determines if it is even.
 // It should return a boolean value - true if it is even, false if it is odd.
 function isEven(num) {
-  if = ;
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Given 2 numbers, generate a random number within that range.
 // Make it inclusive so that the starting and ending numbers are part
 // of the range.HINT ** Utilize Javascript's Math Class (To the Google!)
-function numberGenerator(num1, num2) {}
+function numberGenerator(num1, num2) {
+  return Math.floor(Math.random() * (num1 - num2)) + num2;
+}
