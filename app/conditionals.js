4e030bb44c9a223d5e67c86708aedda12802a945
reedm26 <reedm26@yahoo.com>
2019-11-12 18:49:13 -0700

    working on fever

--- app/conditionals.js
+++ app/conditionals.js
@@ -51,7 +51,15 @@ function timeOfDayGreeting(hour) {
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
-function isFever(temp) {}
+function isFever(temp) {
+  if (temp >= 98.6) {
+    return "fever";
+  } else if (temp >= 103) {
+    return "fever go to hospital";
+  } else if (temp <= 98.6) {
+    return "no fever";
+  }
+}
 
 //5. Write a function that takes in a car object, if it is not moving then return true
 let myCar = {
